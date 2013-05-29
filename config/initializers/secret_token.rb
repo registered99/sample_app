07bcd184cef496b533616a98cf0e976be7041636
registered99 <justin@shadowhealth.com>
2013-05-29 10:37:29 -0400

    Initial Commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '75c721def31055be69962a6bf3cd4e1b05a071d028b93fbd3d387c35b52c6f5e2b53a4c0c1c0da1e35e1ff1f69bc141378d6c2fa03e0a1dc3f81b427b83cf501'
